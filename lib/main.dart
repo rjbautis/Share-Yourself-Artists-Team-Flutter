512ca2c632273e2b0c73fd731e9451fb06c10d98
amabugat <34631534+amabugat@users.noreply.github.com>
2018-10-16 18:16:15 -0700

    Added http get functionality with button interactivity using FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,27 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
 
 void main() => runApp(new MyApp());
 
@@ -44,17 +67,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _display = "";
   var list = [10];
 
-  void _incrementCounter() {
+  Widget futureWidgetGet() {
+    return new FutureBuilder<String>(builder: (context, snapshot) {
+      // if(snapshot.hasData){return new Text(display);}    //does not display updated text
+      if (_display != null) {
+        return new Text(_display);
+      }
+      return new Text("no data yet");
+    });
+  }
+
+  Future<Null> _create() async { //<> means return type, Null = void
+    http.Response response = await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"), //This JSON file is an array
+        headers: {"Accept": "application/json"});
+    List data = json.decode(response.body);
+    print(data[2]['userId'].toString());
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _display = data[2]['userId'].toString();
+    });
+  }
+
+  Future<Null> _read() async { //<> means return type, Null = void
+    http.Response response = await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"), //This JSON file is an array
+        headers: {"Accept": "application/json"});
+    List data = json.decode(response.body);
+    print(data[2]['id'].toString());
+    setState(() {
+      _display = data[2]['id'].toString();
+    });
+  }
+
+  Future<Null> _update() async { //<> means return type, Null = void
+    http.Response response = await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"), //This JSON file is an array
+        headers: {"Accept": "application/json"});
+    List data = json.decode(response.body);
+    print(data[2]['title'].toString());
+    setState(() {
+      _display = data[2]['title'].toString();
+    });
+  }
+
+  Future<Null> _delete() async { //<> means return type, Null = void
+    http.Response response = await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"), //This JSON file is an array
+        headers: {"Accept": "application/json"});
+    List data = json.decode(response.body);
+    print(data[2]['body'].toString());
+    setState(() {
+      _display = data[2]['body'].toString();
     });
   }
 
@@ -89,53 +155,56 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-
+            new Text("Function Call: $_display"),
             new MaterialButton(
-              child: const Text('Add'),
+              child: const Text('Create'),
               color: Colors.red,
               textColor: Colors.white,
               elevation: 4.0,
-              onPressed: _incrementCounter,
+              onPressed: _create,
+              minWidth: 200.0,
+              height: 50.0,//Need to add onPressed event in order to make button active
+            ),
+            new MaterialButton(
+              child: const Text('Read'),
+              color: Colors.purple,
+              textColor: Colors.white,
+              elevation: 4.0,
+              onPressed: _read,
               minWidth: 200.0,
               height: 50.0,//Need to add onPressed event in order to make button active
             ),
             new MaterialButton(
               child: const Text('Update'),
-              color: Colors.purple,
+              color: Colors.green,
               textColor: Colors.white,
               elevation: 4.0,
-              onPressed: _incrementCounter,
+              onPressed: _update,
               minWidth: 200.0,
               height: 50.0,//Need to add onPressed event in order to make button active
             ),
             new MaterialButton(
               child: const Text('Delete'),
-              color: Colors.green,
-              textColor: Colors.white,
-              elevation: 4.0,
-              onPressed: _incrementCounter,
-              minWidth: 200.0,
-              height: 50.0,//Need to add onPressed event in order to make button active
-            ),
-            new MaterialButton(
-              child: const Text('Fetch'),
               color: Colors.blue,
               textColor: Colors.white,
               elevation: 4.0,
-              onPressed: _incrementCounter,
+              onPressed: _delete,
               minWidth: 200.0,
               height: 50.0,//Need to add onPressed event in order to make button active
             ),
+            //futureWidgetPost()
           ],
         ),
-      ),
-//      floatingActionButton: new FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: new Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+//HTTP Notes
+//200+ means the request has succeeded.
+//300+ means the request is redirected to another URL
+//400+ means an error that originates from the client has occurred
+//500+ means an error that originates from the server has occurred
+
