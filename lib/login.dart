f9b9bde2fa3fa98453be1ab5f49133a4bdc6e1a5
Ryan Bautista <rjbautis1@gmail.com>
2018-10-21 20:25:28 -0700

    Working login screen

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'signup.dart';
 
 class LoginPage extends StatefulWidget {
