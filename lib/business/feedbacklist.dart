c2fadf01b58f6b0e6241013fbd5f8da9f8d8e3c5
fhampp <fhampp@gmail.com>
2018-11-13 19:40:51 -0800

    fixed feedbacklist binding

--- lib/business/feedbacklist.dart
+++ lib/business/feedbacklist.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:share_yourself_artists_team_flutter/authentication/authentication.dart';
 import 'package:share_yourself_artists_team_flutter/authentication/inMemory.dart';
