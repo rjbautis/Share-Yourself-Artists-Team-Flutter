6b31e8e7d285508a6210227348d5847f91b2fd46
Ryan Bautista <rjbautis1@gmail.com>
2018-11-16 11:28:21 -0800

    * Introduced inMemory.dart, which handles all persistent memory cases (#24)
    * Introduced named routes

---
+++ lib/routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:share_yourself_artists_team_flutter/artist/artist-upload-image.dart';
+import 'package:share_yourself_artists_team_flutter/authentication/artistSignUp.dart';
+import 'package:share_yourself_artists_team_flutter/authentication/businessSignUp.dart';
+import 'package:share_yourself_artists_team_flutter/authentication/login.dart';
+import 'package:share_yourself_artists_team_flutter/business/feedbacklist.dart';
+
+final routes = {
+  '/login': (BuildContext context) => new LoginPage(),
+  '/artist': (BuildContext context) => new ArtistUploadImage(),
+  '/artistSignUp': (BuildContext context) => new ArtistSignUpPage(),
+  '/business': (BuildContext context) => new FeedbackList(),
+  '/businessSignUp': (BuildContext context) => new BusinessSignUpPage(),
+  '/': (BuildContext context) => new LoginPage()
+};
