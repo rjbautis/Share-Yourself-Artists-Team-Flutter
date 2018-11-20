663d9e796863c7becb0b6bc3f17975f62f28e022
David Lang <david3de@gmail.com>
2018-11-20 01:51:42 -0800

    Reconfigured some routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,7 @@ import 'package:share_yourself_artists_team_flutter/authentication/artistSignUp.
 import 'package:share_yourself_artists_team_flutter/authentication/businessSignUp.dart';
 import 'package:share_yourself_artists_team_flutter/authentication/login.dart';
 import 'package:share_yourself_artists_team_flutter/business/feedbacklist.dart';
+import 'package:share_yourself_artists_team_flutter/business/editbusiness.dart';
 
 final routes = {
   '/login': (BuildContext context) => new LoginPage(),
@@ -11,5 +12,6 @@ final routes = {
   '/artistSignUp': (BuildContext context) => new ArtistSignUpPage(),
   '/business': (BuildContext context) => new FeedbackList(),
   '/businessSignUp': (BuildContext context) => new BusinessSignUpPage(),
+  '/editBusiness': (BuildContext context) => new EditBusiness(),
   '/': (BuildContext context) => new LoginPage()
 };
