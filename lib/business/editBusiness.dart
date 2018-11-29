36f9270b19614660f7749066d271552d491c936e
David Lang <david3de@gmail.com>
2018-11-28 21:12:32 -0800

    Have to rebase again

--- lib/business/editBusiness.dart
+++ lib/business/editBusiness.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:share_yourself_artists_team_flutter/authentication/inMemory.dart';
 
 class EditBusiness extends StatefulWidget {
@@ -11,16 +11,15 @@ class EditBusiness extends StatefulWidget {
 
 class _EditBusinessState extends State<EditBusiness> {
   String _uid;
+  TextEditingController _controller = new TextEditingController();
+
   String _businessName;
-  String _email;
-  String _about;
-  String _additionalNotes;
-  String _worthKnowing;
-  String _theGood;
   String _publication;
-  String _facebookUrl;
-  String _instagramUrl;
-  String _tumblrUrl;
+  String _followerCount;
+  String _website;
+  String _about;
+  String _worthKnowing;
+  String _additionalNotes;
 
   @override
   void initState() {
@@ -33,29 +32,89 @@ class _EditBusinessState extends State<EditBusiness> {
         _uid = uid;
       });
     });
-    _getProfile();
   }
 
-  Future _getProfile() async {
-    DocumentSnapshot businessUser =
-        await Firestore.instance.collection('users').document(_uid).get();
-
+  Future _getProfile(AsyncSnapshot<QuerySnapshot> snapshot) async {
     setState(() {
-      _businessName = businessUser['business_name'];
-      _email = businessUser['email'];
-      _facebookUrl = businessUser['facebook_url'];
-      _instagramUrl = businessUser['instagram_url'];
-      _publication = businessUser['publication'];
-      _about = businessUser['about'];
-      _additionalNotes = businessUser['additional_notes'];
-      _tumblrUrl = businessUser['tumblr_url'];
-      _worthKnowing = businessUser['worth_knowing'];
-      _theGood = businessUser['theGood'];
+      _businessName = snapshot.data.documents[0]['business_name'].toString();
+      _publication = snapshot.data.documents[0]['publication'].toString();
+      _followerCount = snapshot.data.documents[0]['follower_count'].toString();
+      _website = snapshot.data.documents[0]['website'].toString();
+      _about = snapshot.data.documents[0]['about'].toString();
+      _worthKnowing = snapshot.data.documents[0]['worth_knowing'].toString();
+      _additionalNotes =
+          snapshot.data.documents[0]['additional_notes'].toString();
     });
   }
 
-  void _updateProfile() {
+  void onEditComplete() {
+    setState(() {
+      _businessName = _controller.text;
+    });
+  }
 
+  Widget _buildProfile(AsyncSnapshot<QuerySnapshot> snapshot) {
+    _getProfile(snapshot);
+    _controller.addListener(onEditComplete);
+
+    return new Column(children: <Widget>[
+      TextFormField(
+        //controller: _controller,
+        initialValue: _businessName,
+        decoration: InputDecoration(labelText: 'Business Name'),
+        onSaved: (input) => setState(() {
+              _businessName = input;
+            }),
+      ),
+      TextFormField(
+        initialValue: _publication,
+        decoration: InputDecoration(labelText: 'Publication'),
+        onSaved: (input) => _publication = input,
+      ),
+      TextFormField(
+        initialValue: _followerCount,
+        decoration: InputDecoration(labelText: 'Follower Count'),
+        onSaved: (input) => _followerCount = input,
+      ),
+      TextFormField(
+        initialValue: _website,
+        decoration: InputDecoration(labelText: 'Website'),
+        onSaved: (input) => _website = input,
+      ),
+      TextFormField(
+        initialValue: _about,
+        maxLines: 8,
+        decoration: InputDecoration(labelText: 'About'),
+        onSaved: (input) => _about = input,
+      ),
+      TextFormField(
+        initialValue: _worthKnowing,
+        maxLines: 3,
+        decoration: InputDecoration(labelText: 'Worth Knowing'),
+        onSaved: (input) => _worthKnowing = input,
+      ),
+      TextFormField(
+        initialValue: _additionalNotes,
+        maxLines: 3,
+        decoration: InputDecoration(labelText: 'Additional Notes'),
+        onSaved: (input) => _additionalNotes = input,
+      ),
+    ]);
+  }
+
+  Future _updateProfile() async {
+    await Firestore.instance.collection('users').document(_uid).updateData({
+      'business_name': _businessName,
+      'publication': _publication,
+      'follower_count': _followerCount,
+      'website': _website,
+      'about': _about,
+      'worth_knowing': _worthKnowing,
+      'additional_notes': _additionalNotes
+    });
+
+    print('sucessfully updated business profile');
+    print(_businessName);
   }
 
   @override
@@ -73,32 +132,23 @@ class _EditBusinessState extends State<EditBusiness> {
                 Navigator.pop(context);
               },
             ),
-          ]
+          ]),
+      body: new StreamBuilder(
+        stream: Firestore.instance
+            .collection('users')
+            .where('userId', isEqualTo: '${_uid}')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return new Text('Loading...');
+          return new Container(
+            child: ListView.builder(
+              itemBuilder: (BuildContext ctxt, int index) =>
+                  _buildProfile(snapshot),
+              itemCount: 1,
+            ),
+          );
+        },
       ),
-      body: new Column(children: <Widget>[
-        TextFormField(
-          initialValue: _businessName,
-          decoration: InputDecoration(labelText: 'Business Name'),
-          onSaved: (input) => _businessName = input,
-        ),
-        TextFormField(
-          initialValue: _email,
-          decoration: InputDecoration(labelText: 'Email'),
-          onSaved: (input) => _email = input,
-        ),
-        TextFormField(
-          initialValue: _about,
-          maxLines: 8,
-          decoration: InputDecoration(labelText: 'About'),
-          onSaved: (input) => _about = input,
-        ),
-        TextFormField(
-          initialValue: _additionalNotes,
-          maxLines: 3,
-          decoration: InputDecoration(labelText: 'Additional Notes'),
-          onSaved: (input) => _additionalNotes = input,
-        ),
-      ]),
     );
   }
 }
